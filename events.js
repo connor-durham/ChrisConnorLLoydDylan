88f6a3cf06a6791d24894b749a6554030535856c
iuglyboi28 <iuglyboi28@gmail.com>
2019-07-20 12:31:48 -0400

    added my script

--- events.js
+++ events.js
@@ -1,1 +1,11 @@
+function displayEvents() {
+    var events = $(this).attr("data-name");
+    var queryUrl = "https://api.songkick.com/api/3.0/artists/{artist_id}/calendar.json?apikey={your_api_key}"
 
+// ajax call for the API
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response) { 
+        console.log(response)
+    })
