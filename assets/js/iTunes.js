1a64abc6c11a2e291a417036c293c8424599bce2
Dylan Davis <dylandavispc@gmail.com>
2019-07-25 21:26:19 -0400

    iTunes divs created when searched; Info included in iTunes div; Anchored to album page correctly; NEXT: empty search bar and iTunes div when new band searched.

--- assets/js/iTunes.js
+++ assets/js/iTunes.js
@@ -16,7 +16,38 @@ function iTunesSearch(term) {
     }).then(function(response) {
       console.log(response)
       
+      for ( let i = 0; i < response.results.length; i++) {
+        console.log(response.results[i].artistName)
+        console.log(response.results[i].collectionName)
+        console.log(response.results[i].collectionViewUrl)
+        console.log(response.results[i].artworkUrl100)
+
+        
+
+        let artName = response.results[i].artistName;
+        let albName = response.results[i].collectionName;
+        let tunUrl = response.results[i].collectionViewUrl;
+        let imgUrl = response.results[i].artworkUrl100;
+        let iTunesDiv =$("<a class='albums' href='" + tunUrl + "'>");
+
+        //img tag to display album img
+        let image = $("<img>").attr("src", imgUrl);
+        //h2 tag for artist name
+        let textbox = $("<div>");
+        let name = $("<p>").html(artName)
+        let album = $("<p>").html(albName)
+        iTunesDiv.append(image)
+        textbox.append(name)
+        textbox.append(album)
+        iTunesDiv.append(textbox)
+        //p tag for album name
+        //adds click function to img so links to iTunes URL
+
+        //append entire iTunes div
+        $("#iTunes").append(iTunesDiv);
+      }
     });
+    
 }
 
 
@@ -26,6 +57,7 @@ function callback() {
 $("#searcher").on("click", function(e) {
     
   e.preventDefault()
+  
 
 
   let searchanswer = $("#searchbar").val().trim()
