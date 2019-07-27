7d33925df51e42cf9aa6f18f4a4fed2f0f0b2be4
Dylan Davis <dylandavispc@gmail.com>
2019-07-27 06:46:18 -0400

    iTunes store app fully functional and styled.

--- assets/js/iTunes.js
+++ assets/js/iTunes.js
@@ -27,19 +27,30 @@ function iTunesSearch(term) {
         let artName = response.results[i].artistName;
         let albName = response.results[i].collectionName;
         let tunUrl = response.results[i].collectionViewUrl;
-        let imgUrl = response.results[i].artworkUrl100;
+        let imgUrl = response.results[i].artworkUrl100.replace('100x100','300x300');
         let iTunesDiv =$("<a class='albums' href='" + tunUrl + "'>");
 
         //img tag to display album img
-        let image = $("<img>").attr("src", imgUrl);
+        let image = $("<img class='albumCover'>").attr("src", imgUrl);
         //h2 tag for artist name
-        let textbox = $("<div>");
-        let name = $("<p>").html(artName)
-        let album = $("<p>").html(albName)
+        let textbox = $("<div class='textbox'>");
+        let gradient = $("<div class='gradient'>");
+        let name = $("<p class='albumText'>").html(artName)
+        let album = $("<p class='albumText'>").html(albName)
         iTunesDiv.append(image)
+        iTunesDiv.append(gradient)
         textbox.append(name)
         textbox.append(album)
         iTunesDiv.append(textbox)
+        // let template = `
+        // <a class='albums' href='${tunUrl}'>
+        //   <img class='albumCover" src="${imgUrl}">
+        //   <div class='textBox'>
+        //     <span class='artistName'>${artName}</span>
+        //     <span class='albumName'>${albName}</span>
+        //   </div>
+        // </a>
+        // `
         //p tag for album name
         //adds click function to img so links to iTunes URL
 
